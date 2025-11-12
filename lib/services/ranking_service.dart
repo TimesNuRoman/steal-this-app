b8713927e83d6483dec74409dcd7b6f8d61ad722
TimesNuRoman <125929449+TimesNuRoman@users.noreply.github.com>
2025-11-12 15:46:25 +0300

    Update and rename RankingService.dart to ranking_service.dart

--- lib/services/ranking_service.dart
+++ lib/services/ranking_service.dart
@@ -1,6 +1,6 @@
-import '../models/Idea.dart';
-import '../models/Thief.dart';
-import '../models/Realization.dart';
+import '../models/idea.dart';
+import '../models/profile.dart';
+import '../models/realization.dart';
 
 class RankingService {
   static int calculateIdeaPopularity(Idea idea) {
@@ -8,11 +8,11 @@ class RankingService {
     score += idea.stealCount * 10;
     score += idea.fameScore * 5;
     score += idea.chain.length * 15;
-    score += (idea.createdAt.isAfter(DateTime.now().subtract(Duration(days: 7))) ? 20 : 0);
+    score += (idea.createdAt.isAfter(DateTime.now().subtract(const Duration(days: 7))) ? 20 : 0);
     return score;
   }
 
-  static int calculateThiefRespect(Thief thief) {
+  static int calculateThiefRespect(Profile thief) {
     int respect = 1;
     respect += (thief.stolenIdeasCount ~/ 5);
     respect += (thief.realizedCount ~/ 2);
@@ -23,7 +23,7 @@ class RankingService {
   static int calculateRealizationScore(Realization realization) {
     int score = 0;
     score += realization.likes * 5;
-    score += (realization.createdAt.isAfter(DateTime.now().subtract(Duration(days: 3))) ? 10 : 0);
+    score += (realization.createdAt.isAfter(DateTime.now().subtract(const Duration(days: 3))) ? 10 : 0);
     return score;
   }
 }
