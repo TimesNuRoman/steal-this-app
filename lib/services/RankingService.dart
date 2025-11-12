0afb70c49a998519d8b570f28637f1c8ca5bfa29
TimesNuRoman <125929449+TimesNuRoman@users.noreply.github.com>
2025-11-12 13:15:36 +0300

    Create RankingService.dart

---
+++ lib/services/RankingService.dart
@@ -0,0 +1,29 @@
+import '../models/Idea.dart';
+import '../models/Thief.dart';
+import '../models/Realization.dart';
+
+class RankingService {
+  static int calculateIdeaPopularity(Idea idea) {
+    int score = 0;
+    score += idea.stealCount * 10;
+    score += idea.fameScore * 5;
+    score += idea.chain.length * 15;
+    score += (idea.createdAt.isAfter(DateTime.now().subtract(Duration(days: 7))) ? 20 : 0);
+    return score;
+  }
+
+  static int calculateThiefRespect(Thief thief) {
+    int respect = 1;
+    respect += (thief.stolenIdeasCount ~/ 5);
+    respect += (thief.realizedCount ~/ 2);
+    respect += (thief.totalFame ~/ 10);
+    return respect.clamp(1, 10);
+  }
+
+  static int calculateRealizationScore(Realization realization) {
+    int score = 0;
+    score += realization.likes * 5;
+    score += (realization.createdAt.isAfter(DateTime.now().subtract(Duration(days: 3))) ? 10 : 0);
+    return score;
+  }
+}
